f49b00dea740c22ba51b501e32cdfe8c6ed0a8ad
ssk333 <sasaki120ml@gmail.com>
2016-09-06 18:41:10 +0900

    user.rbにset_imageメソッドを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,12 @@ has_many :tweets
 
   validates :name, presence: true
   validates :email, presence: true,uniqueness: true
+
+  def set_image(file)
+  if !file.nil?
+    file_name = file.original_filename
+    File.open("public/user_images/#{file_name}", 'wb'){|f| f.write(file.read)}
+    self.image = file_name
+  end
+end
 end
